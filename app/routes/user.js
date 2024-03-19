9d7961d4388950dbf5216a8efa9be19a62087874
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2024-03-19 14:50:00 +0100

    controller route

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express();
+const userCtrl = require("../controllers/user.js");
 
-router.post("/signup", function (req, res) {
-  res.send("You are signup");
-});
-router.post("/login", function (req, res) {
-  res.send("You are login");
-});
+router.post("/signup", userCtrl.signup);
+
+router.post("/login", userCtrl.login);
+
+
+// router.post("/signup", function (req, res) {
+//   res.send("You are signup");
+// });
+// router.post("/login", function (req, res) {
+//   res.send("You are login");
+// });
 
 module.exports = router;
