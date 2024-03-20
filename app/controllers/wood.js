b2426f0c4ad995788f3ada745715519a72a9df59
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2024-03-20 10:02:11 +0100

    hash password update seeder

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -3,9 +3,7 @@ const { Wood } = require('../models');
 exports.listwood = async(req, res) => {
     try{
         const woods = await Wood.findAll();
-        console.log(woods.every(user => user instanceof Wood)); // true
-        console.log("All woods:", JSON.stringify(woods, null, 2));
-        res.status(201).json(woods);
+        res.status(200).json(woods);
 
    }catch(err){
         res.status(500).json({
