a1ea2a5046a55cf888da91b34bbb3b923c8e913b
Farmata-sidibe <69140880+Farmata-sidibe@users.noreply.github.com>
2024-03-22 16:54:07 +0100

    port commit

--- app/config/config.js
+++ app/config/config.js
@@ -6,20 +6,25 @@ module.exports={
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: process.env.DB_USERNAME_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_DATABASE_TEST,
+    host: process.env.DB_HOST_TEST,
+    dialect: process.env.DB_DIALECT_TEST,
+    port: process.env.DB_PORT_TEST
+
   },
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+
   }
 }
